17ef1647a1585012b3b7865bd2c4c115a1c67dcb
Elle Choi <ellehanna@Elles-Mac-Studio.local>
2024-11-14 00:45:56 -0600

    app and procfile

--- app/__init__.py
+++ app/__init__.py
@@ -1,85 +1,53 @@
+# __init__.py 내용
 from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf.csrf import CSRFProtect
 from flask import send_from_directory
-
-
 import os
-
 import logging
 from logging.handlers import RotatingFileHandler
 from dotenv import load_dotenv
 
-
-
-
-
 csrf = CSRFProtect()
-
 db = SQLAlchemy()
 login_manager = LoginManager()
 migrate = Migrate()
 
-
 def create_app():
     app = Flask(__name__)
     csrf.init_app(app)
-
-    # .env 파일에서 환경 변수 로드
     load_dotenv()
-
-    # 비밀 키 및 데이터베이스 URI 설정
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///default.db')
-
-    # 파일 업로드 폴더 설정
     app.config['UPLOAD_FOLDER'] = os.path.join(app.root_path, 'static', 'uploads')
     app.config['THUMBNAIL_FOLDER'] = os.path.join(app.root_path, 'static', 'thumbnails')
 
-    # 폴더가 없는 경우 생성
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    
     if not os.path.exists(app.config['THUMBNAIL_FOLDER']):
         os.makedirs(app.config['THUMBNAIL_FOLDER'])
         app.logger.info(f'Thumbnail folder created at {app.config["THUMBNAIL_FOLDER"]}')
 
-
-
-
-    # 로깅 설정
     if not app.debug:
         file_handler = RotatingFileHandler('error.log', maxBytes=10240, backupCount=10)
         file_handler.setLevel(logging.ERROR)
         app.logger.addHandler(file_handler)
 
-    # CSRF 보호 활성화
-
-    # db 초기화
     db.init_app(app)
     migrate.init_app(app, db)
-
-    # Flask-Login 초기화
     login_manager.init_app(app)
-    login_manager.login_view = 'main.login'  # 로그인 뷰 설정
+    login_manager.login_view = 'main.login'
 
-    # 사용자 로드 함수
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    # 모델 임포트
     from .models import User
-
-    # 라우트 임포트
-    from .routes import bp as main_bp  # 블루프린트 다시 'main'으로 임포트
-    app.register_blueprint(main_bp)  # URL 프리픽스 없이 등록
-
-
-
-
-
+    from .routes import bp as main_bp
+    app.register_blueprint(main_bp)
 
     return app
+
+app = create_app()  # 전역 app 객체 생성
